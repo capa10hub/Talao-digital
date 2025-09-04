d57d36ce373ba23b0bec4ba5970731b3f053389d
gabriel <gabrielcampos4545@gmail.com>
2025-09-04 19:06:18 -0300

    Ultimos campos

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-    for (var i = 1; i <= 6; i++) {
-    const quantidade = document.getElementById("quantidade" + i);
-    const modelo = document.getElementById("modelo" + i);
-    const unidade = document.getElementById("unidade" + i);
-    const total = document.getElementById("total" + i);
+    for (var i = 1; i <= 8; i++) {
+        const quantidade = document.getElementById("quantidade" + i);
+        const modelo = document.getElementById("modelo" + i);
+        const unidade = document.getElementById("unidade" + i);
+        const total = document.getElementById("total" + i);
 
-    modelo.addEventListener("change", function() {
-        const valorUnidade = parseFloat(modelo.value);
-        unidade.value = valorUnidade.toFixed(2);
-        total.value = (quantidade.value * valorUnidade).toFixed(2);
-        calcularTotalGeral();
-    });
+        modelo.addEventListener("change", function() {
+            const valorUnidade = parseFloat(modelo.value);
+            unidade.value = valorUnidade.toFixed(2);
+            if (quantidade.value !== "") {
+                total.value = (quantidade.value * valorUnidade).toFixed(2);
+            }
+            calcularTotalGeral();
+        });
 
-    quantidade.addEventListener("input", function() {
-        const valorUnidade = parseFloat(modelo.value);
-        total.value = (quantidade.value * valorUnidade).toFixed(2);
-        calcularTotalGeral();
-    });
+        quantidade.addEventListener("input", function() {
+            const valorUnidade = parseFloat(modelo.value);
+            if (valorUnidade && quantidade.value !== "") {
+                total.value = (quantidade.value * valorUnidade).toFixed(2);
+            }
+            calcularTotalGeral();
+        });
     }
 
     function calcularTotalGeral() {
-    let totalGeral = 0;
-    for (var i = 1; i <= 6; i++) {
-        const total = document.getElementById("total" + i);
-        if (total.value !== "") {
-        totalGeral += parseFloat(total.value);
+        let totalGeral = 0;
+        for (var i = 1; i <= 8; i++) {
+            const total = document.getElementById("total" + i);
+            if (total.value !== "") {
+                totalGeral += parseFloat(total.value);
+            }
         }
-    }
-    document.getElementById("totalGeral").value = totalGeral.toFixed(2);
+        document.getElementById("totalGeral").value = totalGeral.toFixed(2);
     }
 });
